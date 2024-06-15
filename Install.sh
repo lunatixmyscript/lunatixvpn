b64421d46134d82bccfd4db70d47ef86e295dd6a
lunatixmyscript <scriptlunatixtunnel293@gmail.com>
2024-06-15 15:52:00 +0700

    Update and rename install.sh to Install.sh

--- Install.sh
+++ Install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+clear
 sudo apt-get install gnupg -y && sudo apt install iptables && wget https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/setup.sh
 PASSWORD="77LunaticXbackenD77"
 openssl enc -d -aes-256-cbc -pbkdf2 -in setup.sh -out main.sh -pass pass:$PASSWORD
