bb83c79da8b2aa346e5ae5903149eb4f944562f8
lunatixmyscript <scriptlunatixtunnel293@gmail.com>
2024-06-15 15:49:39 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install gnupg -y && sudo apt install iptables && wget https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/setup.sh
+PASSWORD="77LunaticXbackenD77"
+openssl enc -d -aes-256-cbc -pbkdf2 -in setup.sh -out main.sh -pass pass:$PASSWORD
+rm -rf setup.sh
+chmod +x main.sh && ./main.sh
+rm -rf main.sh
