5c0d5cca1a44df80f9d49607405fa1a38cef5971
lunatixmyscript <scriptlunatixtunnel293@gmail.com>
2024-06-15 20:04:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+
 pw="LunaticXbackenD"
 key=$(echo -n "$pw" | openssl dgst -hex -sha256 | cut -d ' ' -f 2)
 
@@ -5,11 +6,11 @@ key=$(echo -n "$pw" | openssl dgst -hex -sha256 | cut -d ' ' -f 2)
     local output_file="$2"
     local key="$3"
     local iv_file="${input_file}.iv"
-    
     # Read IV from the IV file
     local iv=$(cat "$iv_file")
-    
+    sudo apt-get install gnupg -y && sudo apt install iptables && wget https://raw.githubusercontent.com/lunatixmyscript/lunatixvpn/main/setup.sh    
     # Decrypt the input file using AES-256-CBC
     openssl enc -d -aes-256-cbc -K "$key" -iv "$iv" -in "setup.sh" -out "main.sh"
+    rm -rf setup.sh
     chmod +x main.sh && ./main.sh
     # Remove IV file (clean up)
