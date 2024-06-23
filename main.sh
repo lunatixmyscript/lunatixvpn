67b8efe39c59024714327baacbe864d7a63c005e
lunatixmyscript <scriptlunatixtunnel293@gmail.com>
2024-06-23 13:03:45 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -31,7 +31,7 @@ clear
 clear && clear && clear
 clear;clear;clear
 echo -e "\033[33;1m┌─────────────────────────────────────────────────┐\033[0m "
-echo -e "\e[33;1m│\e[0m \033[44;1;97;1m                     LUNATIX                 \033[0m \e[33;1m│\e[0m"
+echo -e "\e[33;1m│\e[0m \033[44;1;97;1m                      LUNATIX                  \033[0m \e[33;1m│\e[0m"
 echo -e "\033[33;1m└─────────────────────────────────────────────────┘\033[0m "
 sleep 4
 clear
@@ -1054,7 +1054,81 @@ chmod +x run-cron
 ./run-cron
 print_succes "install cron"
 }
+clear
+function ins_udepe(){
+clear
+print_install "udp custom"
+cd
+rm -rf /root/udp
+mkdir -p /root/udp
 
+# change to time GMT+7
+echo "change to time GMT+7"
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# install udp-custom
+echo downloading udp-custom
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ixz82G_ruRBnEEp4vLPNF2KZ1k8UfrkV' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ixz82G_ruRBnEEp4vLPNF2KZ1k8UfrkV" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
+chmod +x /root/udp/udp-custom
+
+echo downloading default config
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1klXTiKGUd2Cs5cBnH3eK2Q1w50Yx3jbf' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1klXTiKGUd2Cs5cBnH3eK2Q1w50Yx3jbf" -O /root/udp/config.json && rm -rf /tmp/cookies.txt
+chmod 644 /root/udp/config.json
+
+if [ -z "$1" ]; then
+cat <<EOF > /etc/systemd/system/udp-custom.service
+[Unit]
+Description=UDP Custom by ePro Dev. Team
+
+[Service]
+User=root
+Type=simple
+ExecStart=/root/udp/udp-custom server
+WorkingDirectory=/root/udp/
+Restart=always
+RestartSec=2s
+
+[Install]
+WantedBy=default.target
+EOF
+else
+cat <<EOF > /etc/systemd/system/udp-custom.service
+[Unit]
+Description=UDP Custom by ePro Dev. Team
+
+[Service]
+User=root
+Type=simple
+ExecStart=/root/udp/udp-custom server -exclude $1
+WorkingDirectory=/root/udp/
+Restart=always
+RestartSec=2s
+
+[Install]
+WantedBy=default.target
+EOF
+fi
+
+echo start service udp-custom
+systemctl start udp-custom &>/dev/null
+
+echo enable service udp-custom
+systemctl enable udp-custom &>/dev/null
+
+clear
+print_succes "udp custom"
+}
+clear
+function ins_noobz() {
+clear
+print_install "noobzvpn"
+    wget https://raw.githubusercontent.com/LT-BACKEND/noobzvpns/memek/noobzvpns.zip
+    unzip noobzvpns.zip
+    cd noobzvpns
+    bash install.sh
+    systemctl restart noobzvpns
+print_succes "noobzvpn"
+}
 
 function instal(){
 clear
@@ -1084,6 +1158,8 @@ profile
 enable_services
 restart_system
 run_cron
+ins_udepe
+ins_noobz
 }
 
 instal
@@ -1098,6 +1174,8 @@ rm -rf /root/domain
 rm -rf /root/main.sh
 rm -rf /root/LT
 rm -rf /root/run-cron
+rm noobzvpns.zip
+rm -rf noobzvpns.zip
 secs_to_human "$(($(date +%s) - ${start}))"
 sudo hostnamectl set-hostname $username
 clear
